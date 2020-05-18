ae3c68a824ac6f8daf7447678eed6e9b1fe6a6e8
Yuehao Li <yli509@ucsc.edu>
2020-05-18 05:22:56 -0700

    DictionaryTest modify

--- pa5/DictionaryTest.c
+++ pa5/DictionaryTest.c
@@ -7,7 +7,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
-#include"Dictionary.c"
+#include"Dictionary.h"
 
 int main(){
     Dictionary D = newDictionary(1);
@@ -21,7 +21,43 @@ int main(){
     delete(D, "c");
 
     printDictionary(stdout,D);
-
-
     
+    printf("\n");
+    insert(U, "a", 45);
+    insert(U, "b", 2);
+    insert(U, "c", 134);
+    insert(U, "d", 543);
+    insert(U, "e", 23);
+    printDictionary(stdout,U);
+    beginForward(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+    next(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+    next(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+    next(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+    
+    beginReverse(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+
+    prev(U);
+    printf("%s  ",currentKey(U));
+    printf("%d\n", currentVal(U));
+
+    printf("Before Delete:\n");
+    printDictionary(stdout,U);
+    delete(U, "c");
+    delete(U, "d");
+    printf("After Delete:\n");
+    printDictionary(stdout,U);
+
+    freeDictionary(&D);
+    freeDictionary(&U);
+    return 0;
 }
